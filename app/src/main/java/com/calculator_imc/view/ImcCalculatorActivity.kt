59056ea0e8053c31b3c8ce54398d1837a622cb04
Raydberg Gabriel <raydbergg@gmail.com>
2025-05-25 12:02:52 -0500

    Add IMC calculator layout and resources for gender selection

--- app/src/main/java/com/calculator_imc/view/ImcCalculatorActivity.kt
+++ app/src/main/java/com/calculator_imc/view/ImcCalculatorActivity.kt
@@ -3,19 +3,67 @@ package com.calculator_imc.view
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.calculator_imc.R
 
 class ImcCalculatorActivity : AppCompatActivity() {
+    private var isMaleSelected: Boolean = true;
+    private var isFemaleSelected: Boolean = false;
+
+    // lateinit -> Se inicia cuando yo le diga , ya que esta afuera
+    private lateinit var viewMale: CardView;
+    private lateinit var viewFemale: CardView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_imc_calculator)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        initComponent()
+        initListeners()
+        initUi()
+
+    }
+
+    private fun initUi() {
+        setGenderColor()
+    }
+
+    private fun initComponent() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
         }
     }
+
+    private fun changeGender() {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isMaleSelected
+    }
+
+    private fun setGenderColor() {
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+    }
+
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+        val colorReference = if (isSelectedComponent) {
+            R.color.background_component_select
+        } else {
+            R.color.background_component
+        }
+        return ContextCompat.getColor(this, colorReference)
+    }
+
 }
